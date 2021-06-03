f1a2e3734619c7d24353e1b7998f480c77fd3076
InduPriya Pokuri <34738280+InduPriya-pokuri@users.noreply.github.com>
2021-06-03 17:38:08 +0530

    by InduPriya

---
+++ package/module.py
@@ -0,0 +1,23 @@
+def sum(a,b):
+    return a+b
+
+sum(10,20)
+
+def sub(c,d):
+    return c-d
+sub(30,40)
+
+def mul(e,f):
+    return e*f
+mul(5,50)
+
+
+
+def isleap1(year):
+    if year%4==0 :
+        return True
+    else:
+        return False  
+    
+isleap1(2020)
+        
